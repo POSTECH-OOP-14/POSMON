2aef1b12984a8f2e9707e572f2591b99bea53183
MoonCall <moonchang@postech.ac.kr>
2015-11-08 12:41:28 +0900

    First Project Interface (Player in Field & Start Scene)

---
+++ POSMON_Project/Assets/GameLoad.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    public void ChangeScene()
+    {
+        Application.LoadLevel(1);
+    }
+}
