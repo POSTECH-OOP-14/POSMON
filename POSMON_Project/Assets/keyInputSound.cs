c97cfe57347e1c9255c1e234277abe7e4b72834f
SongSiHo <wt6248@gmail.com>
2015-12-04 18:50:30 +0900

    전투시 마우스 키를 눌렀을 때 버튼 소리가 나타난다.

---
+++ POSMON_Project/Assets/keyInputSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyInputSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+            gameObject.GetComponent<AudioSource>().PlayOneShot(gameObject.GetComponent<AudioSource>().clip);
+	}
+}
